c051c16ce84c4dde86d08651f28fd6d7a8679ed2
yajan-singh <yajan08@gmail.com>
2020-05-03 17:52:32 -0400

    l

--- Game-1/main.lua
+++ Game-1/main.lua
@@ -1,1 +1,18 @@
- 
+function love.load()
+	button = {}
+	button.x = 200
+	button.y = 800
+	button.width = 100
+	button.length = 800
+end
+
+
+function  love.update(dt)
+	-- body
+end
+
+
+function love.draw()
+	love.graphics.setColor(255, 255, 1)
+	love.graphics.rectangle("fill", button.x, button.y, button.length, button.width)
+end
